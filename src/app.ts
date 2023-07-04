778609b179933cd755262a1d043f82460a8f7ec7
achaparro <achmarmol@gmail.com>
2023-07-04 19:54:53 -0400

    feat: agregado de archivo app con configuracion de instancia de aplicacion express, uso de middlewares/funcionalidades y uso de archivo index.ts para el arranque de la API
    se esta usando el bodyParser para convertir la peticion que llega del
    front como un json a un obejto json de javaScript y poder manejar este
    desde las peticiones de los controladores.
    
    se establece el coors para permitir el usuo de la API desde distintos
    origines
    
    y por ultimo se implementa morgan para ver la peticiones realizadas a la
    API y el resultado de la respuesta

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+
+// create a express aplication insatnce
+const app = express();
+
+// body parser to work with json files
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(express.json());
+
+// set cors with origin '*' and credentials
+app.use(cors({ origin: true, credentials: true }));
+
+// use morgan for monitoring requests
+app.use(morgan('dev'));
+
+export default app;
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import app from './app'
+import { config } from './config/config-loader';
+
+
+// init application function
+(async () => {
+    try {
+        await app.listen(config.PORT, () => {
+            console.log(`API listening on port: ${config.PORT}`)
+            console.log("**** Let's to play :) ****")
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})();
