d463c5e331bfdb325922429b47c2c28581f98d16
achaparro <achmarmol@gmail.com>
2023-07-05 21:57:54 -0400

    fix: arreglo de validacion para verificar que Los buckets sean mayores o iguales al valor de la meta Z, anteriormente se verifica que la suma de estos dos sea mayor a Z

--- src/helpers/hasSolution.ts
+++ src/helpers/hasSolution.ts
@@ -3,7 +3,7 @@ import { IProblemBody } from "../interfaces"
 export const problemHasSolution = ({ bucketX, bucketY, goalZ }:IProblemBody):boolean => {
 
     // The buckets must be greater than or equal to the value of the goal Z
-    if (bucketX + bucketY < goalZ) return false;
+    if (bucketX < goalZ && bucketY < goalZ) return false;
 
     // validate bucket X and Bucket Y is not equal
     if (bucketX === bucketY) return false;
